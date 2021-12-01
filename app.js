638884b0c71ef2b2337efae8c587dee21a9011a9
Ritik Shah <79132559+ritik-1302@users.noreply.github.com>
2021-12-02 00:45:29 +0530

    EJs Done

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app=express();
+const date=require(__dirname+"/date.js");
+
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+let items=[
+    "Buy Food",
+    "Cook Food",
+    "Eat Food" 
+
+];
+let workItems=[
+
+];
+
+app.get("/",function(req,res){
+
+let day=date.getDate();
+
+
+
+
+res.render("list",{listTitle:day,newListItems:items});
+
+
+});
+
+
+app.post("/",function(req,res){
+
+ let item= req.body.newItem;
+  
+ if(req.body.list==="Work"){
+     workItems.push(item);
+     res.redirect("/work");
+
+ }else{
+     
+     items.push(item);
+     res.redirect("/");
+ }
+
+
+
+});
+
+
+
+app.get("/work",function(req,res){
+
+    res.render("list",{listTitle:"Work",newListItems:workItems})
+
+});
+
+
+
+app.listen(3000,function(){
+
+    console.log("Server is running on the port 3000");
+});
+
